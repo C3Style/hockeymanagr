a8c4f45d31eb30082a5fb0f80b6bb807cbbcaa42
ADI\lugonch <christophe.lugon@fhvi.ch>
2019-03-06 16:10:36 +0100

    a

--- server.js
+++ server.js
@@ -2,20 +2,25 @@
 const express = require('express');
 const path = require('path');
 const request = require('request');
+// const cors = require('cors');
 
 const app = express();
+
+// Enable CORS
+// app.use(cors());
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+app.get('/api/*', function(req,res) {
+	var newurl = 'https://hockeymanager.ch';
+	request(newurl).pipe(res);
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/hm'));
 
 app.get('/*', function(req,res) {
 	res.sendFile(path.join(__dirname+'/dist/hm/index.html'));
-});
-
-app.get('/api/*', function(req,res) {
-	var newurl = 'https://hockeymanager.ch';
-	request(newurl).pipe(res);
-	// res.sendFile(path.join(__dirname+'/dist/hm/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
